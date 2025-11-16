6ebc23dac4af2225193dd771879daef6e6886d46
mau-dot <pg23022@ues.edu.sv>
2025-11-16 12:41:53 -0600

    Estructura inicial del proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.proyecto_final_prograiii {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.proyecto_final_prograiii to javafx.fxml;
+    exports com.example.proyecto_final_prograiii;
+    exports com.example.proyecto_final_prograiii.controllers;
+    //exports com.example.proyecto_final_prograiii.models;
+
+    opens com.example.proyecto_final_prograiii.controllers to javafx.fxml;
+}
