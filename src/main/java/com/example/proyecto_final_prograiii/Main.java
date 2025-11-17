eb901d7d77947042225a803369d11d60bfb25f42
MauW69 <mauricioguatemala13@gmail.com>
2025-11-17 08:31:09 -0600

    cambio el titulo en el login

--- src/main/java/com/example/proyecto_final_prograiii/Main.java
+++ src/main/java/com/example/proyecto_final_prograiii/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 592, 405);
-        stage.setTitle("LOGIN");
+        stage.setTitle("login");
         stage.setScene(scene);
         stage.show();
     }
