01951fb5b447483de3e58c30e8b7e926cb12d745
Carlos-Lapa <BL23012@ues.edu.sv>
2025-11-18 12:59:23 -0600

    CREACION DE RENTA CAR CONTROLLER, CONFIGURACION DE BOTON INVITADO

--- src/main/java/com/example/proyecto_final_prograiii/controllers/LoginController.java
+++ src/main/java/com/example/proyecto_final_prograiii/controllers/LoginController.java
@@ -2,9 +2,15 @@ package com.example.proyecto_final_prograiii.controllers;
 
 import com.example.proyecto_final_prograiii.config.ConexionDB;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -46,6 +52,20 @@ public class LoginController {
 
     @FXML
     void ModoInvitado(ActionEvent event) {
+        btnModoInvitado.getScene().getWindow().hide();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/proyecto_final_prograiii/rentacar-view.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            // Obtener la ventana actual desde el botón btnModoInvitado
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("RENTA CAR");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
